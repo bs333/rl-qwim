70d868dad35600914fb703770ae5dbfe7c821e1e
Sid Bhatia <siddharth.bhatia789@gmail.com>
2023-11-26 18:51:00 -0500

    Add build_actor() method.

--- PPO.py
+++ PPO.py
@@ -3,6 +3,7 @@ from tensorflow.keras.layers import Input, Dense
 from tensorflow.keras.optimizers import Adam
 
 class PPO:
+
     def __init__(self, state_dim, action_dim, actor_lr, critic_lr, clip_ratio):
         self.state_dim = state_dim
         self.action_dim = action_dim
@@ -15,4 +16,17 @@ class PPO:
         
         self.actor_optimizer = Adam(learning_rate=self.actor_lr)
         self.critic_optimizer = Adam(learning_rate=self.critic_lr)
+
+    def build_actor(self):
+        state_input = Input(shape=(self.state_dim,))
+        advantages = Input(shape=(1,))
+        old_prediction = Input(shape=(self.action_dim,))
+
+        x = Dense(64, activation='relu')(state_input)
+        x = Dense(64, activation='relu')(x)
+        action_probs = Dense(self.action_dim, activation='softmax')(x)
+
+        model = tf.keras.models.Model(inputs=[state_input, advantages, old_prediction], outputs=action_probs)
+        return model
+
 
