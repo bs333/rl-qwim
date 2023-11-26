b899f3bd09392d99c293832a9aa409dedb7f0d87
melissatmaca <melissaatmaca@outlook.com>
2023-11-26 15:36:29 -0500

    Create PPO.py

---
+++ figures/PPO.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+class Memory:
+    
+    def __init__(self, batch_size):
+        self.states = []
+        self.probs = []
+        self.vals = []
+        self.actions = []
+        self.rewards = []
+        self.batch_size = []
