ab998a72b519c6f5cc949f92f6979a195f0ce6c2
Sid Bhatia <siddharth.bhatia789@gmail.com>
2023-11-26 20:37:03 -0500

    Create PO class.

--- portfolio_optimization.py
+++ portfolio_optimization.py
@@ -2,3 +2,14 @@ import yfinance as yf
 import pandas as pd
 import matplotlib.pyplot as plt
 from statsmodels.tsa.stattools import adfuller
+
+class PortfolioOptimization:
+    """
+    A class for portfolio optimization using financial data.
+
+    Attributes:
+        tickers (List[str]): List of ticker symbols for ETFs.
+        start_date (str): Start date for data retrieval.
+        end_date (str): End date for data retrieval.
+        data (pd.DataFrame): Dataframe holding the financial data.
+    """
