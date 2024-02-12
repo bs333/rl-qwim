32e3e4c4d17be01a634fcb8af957ac6a9677ec1e
Sid Bhatia <siddharth.bhatia789@gmail.com>
2024-02-12 08:49:56 -0500

    Add class and constructor for LR.py.

--- LR.py
+++ LR.py
@@ -5,3 +5,11 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import accuracy_score
+
+class LogisticRegressionPortfolioOptimizer:
+    def __init__(self, tickers, start_date, end_date):
+        self.tickers = tickers
+        self.start_date = start_date
+        self.end_date = end_date
+        self.data = None
+        self.models = {}
