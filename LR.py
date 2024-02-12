5a519cd93110197c25389f3053ace6bfc976da15
Sid Bhatia <siddharth.bhatia789@gmail.com>
2024-02-12 08:49:14 -0500

    Add imports for LR.

---
+++ LR.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd
+import yfinance as yf
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import accuracy_score
