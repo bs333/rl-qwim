ef871ba84ca0b13f14456b4a4256811b55e53d1d
Sid Bhatia <siddharth.bhatia789@gmail.com>
2024-02-12 09:01:55 -0500

    Add 'List' for type-hinting.

--- LR.py
+++ LR.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 import yfinance as yf
-from typing import Dict, Tuple
+from typing import Dict, List, Tuple
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import StandardScaler
